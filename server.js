655e7d52bd5007cfc290b822dae425bcc4d2fe0a
Xeno9948 <stathisxeno@icloud.com>
2026-03-16 15:44:00 +0200

    fix: add urlencoded body parser to handle Mollie webhooks

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ async function getConfig() {
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader('X-Robots-Tag', 'noindex, nofollow');
   next();
